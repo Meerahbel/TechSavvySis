27fd4bc6e9761fe9c4e277ff2ed429bad42f5096
Meerahbel <mirabelugoji@gmail.com>
2024-01-11 00:11:24 -0800

    made minor changes

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -2,13 +2,13 @@ const Works = () => {
   return (
     <section className="pb-40 px-3 md:px-[60px]">
       <div className="flex gap-10 items-center">
-        <div className="h-[1px] w-[200px] lg:w-[400px] bg-[#ec4899]"></div>
+        <div className="h-[1px] w-[200px] lg:w-[400px] bg-[#ec4899] hidden lg:block"></div>
         <p className="text-[#a6aec2] text-[1.5rem] lg:text-[2rem]">
           <span className="text-[#ec4899]">02.</span> Works
         </p>
         <div className="h-[1px] w-[200px] lg:w-[400px] bg-[#ec4899]"></div>
       </div>
-      <p className="text-[#a6aec2] text-[1.2rem]">
+      <p className="text-[#a6aec2] text-[1.2rem] mt-5">
         A small gallery of some websites I've built.{" "}
         <span className="text-[#ec4899]">
           <a href="https://github.com/Meerahbel?tab=repositories" target="/">
