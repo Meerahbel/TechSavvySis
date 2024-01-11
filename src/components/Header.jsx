ab7fb42a5ddb3e84819981aae6dac62efe896e6e
Meerahbel <mirabelugoji@gmail.com>
2024-01-10 18:37:54 -0800

    App created with header

---
+++ src/components/Header.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  const [dropdown, setDropdown] = useState(false);
+
+  const handleMenu = () => {
+    setDropdown((prev) => !prev);
+  };
+
+  return (
+    <header className="flex py-4 px-3 md:px-[60px] justify-between items-center bg-transparent">
+      <div className="flex gap-3 items-center">
+        <img src="/images/icon2.png" alt="icon" className="w-5 h-5" />
+        <p className="text-[#ec4899] tracking-widest">MIRABEL.</p>
+      </div>
+      <ul className="hidden md:flex md:gap-10 lg:gap-20">
+        <li className="text-[#a6aec2]">
+          <span className="text-[#ec4899]">01.</span> About me
+        </li>
+        <li className="text-[#a6aec2]">
+          <span className="text-[#ec4899]">02.</span> Works
+        </li>
+        <li className="text-[#a6aec2]">
+          <span className="text-[#ec4899]">03.</span> Contact
+        </li>
+        <li className="text-[#a6aec2]">
+          <span className="text-[#ec4899]">04.</span> Resume
+        </li>
+      </ul>
+      <div onClick={handleMenu} className="block md:hidden">
+        <img src="/images/menu.png" alt="menu" className="w-6 h-6" />
+      </div>
+
+      {dropdown && (
+        <ul className="w-full absolute z-10 top-[50px] left-0 flex flex-col items-center bg-[#121418] gap-10 py-10 text-2xl md:hidden slide-right">
+          <li className="text-[#a6aec2] border-b border-white/5 pb-5 w-full text-center">
+            <span className="text-[#ec4899]">01.</span> About me
+          </li>
+          <li className="text-[#a6aec2] border-b border-white/5 pb-5 w-full text-center">
+            <span className="text-[#ec4899]">02.</span> Works
+          </li>
+          <li className="text-[#a6aec2] border-b border-white/5 pb-5 w-full text-center">
+            <span className="text-[#ec4899]">03.</span> Contact
+          </li>
+          <li className="text-[#a6aec2] pb-5 w-full text-center">
+            <span className="text-[#ec4899]">04.</span> Resume
+          </li>
+        </ul>
+      )}
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+
+const Homepage = () => {
+  return (
+    <main className="bg-[#121418]">
+      <Header />
+    </main>
+  );
+};
+
+export default Homepage;
