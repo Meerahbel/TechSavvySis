8f6a85cead0b7ac092c979e035efa83fde06ead9
Meerahbel <mirabelugoji@gmail.com>
2024-01-21 20:08:47 -0800

    made changes

--- src/components/Laptop.jsx
+++ src/components/Laptop.jsx
@@ -2,43 +2,83 @@ const Laptop = () => {
   return (
     <div className="laptop--cont">
       <div className="laptop1">
-        <img src="/images/laptop.png" alt="laptop" />
+        <img
+          src="/images/laptop.png"
+          alt="laptop"
+          classname="xl:w-10 xl:h-10"
+        />
       </div>
 
       <div className="laptop2">
-        <img src="/images/laptop.png" alt="laptop" />
+        <img
+          src="/images/laptop.png"
+          alt="laptop"
+          classname="xl:w-10 xl:h-10"
+        />
       </div>
 
       <div className="laptop3">
-        <img src="/images/laptop.png" alt="laptop" />
+        <img
+          src="/images/laptop.png"
+          alt="laptop"
+          classname="xl:w-10 xl:h-10"
+        />
       </div>
 
       <div className="laptop4">
-        <img src="/images/laptop.png" alt="laptop" />
+        <img
+          src="/images/laptop.png"
+          alt="laptop"
+          classname="xl:w-10 xl:h-10"
+        />
       </div>
 
       <div className="laptop5">
-        <img src="/images/laptop.png" alt="laptop" />
+        <img
+          src="/images/laptop.png"
+          alt="laptop"
+          classname="xl:w-10 xl:h-10"
+        />
       </div>
 
       <div className="laptop6">
-        <img src="/images/laptop.png" alt="laptop" />
+        <img
+          src="/images/laptop.png"
+          alt="laptop"
+          classname="xl:w-10 xl:h-10"
+        />
       </div>
 
       <div className="laptop7">
-        <img src="/images/laptop.png" alt="laptop" />
+        <img
+          src="/images/laptop.png"
+          alt="laptop"
+          classname="xl:w-10 xl:h-10"
+        />
       </div>
 
       <div className="laptop8">
-        <img src="/images/laptop.png" alt="laptop" />
+        <img
+          src="/images/laptop.png"
+          alt="laptop"
+          classname="xl:w-10 xl:h-10"
+        />
       </div>
 
       <div className="laptop9">
-        <img src="/images/laptop.png" alt="laptop" />
+        <img
+          src="/images/laptop.png"
+          alt="laptop"
+          classname="xl:w-10 xl:h-10"
+        />
       </div>
 
       <div className="laptop10">
-        <img src="/images/laptop.png" alt="laptop" />
+        <img
+          src="/images/laptop.png"
+          alt="laptop"
+          classname="xl:w-10 xl:h-10"
+        />
       </div>
     </div>
   );
